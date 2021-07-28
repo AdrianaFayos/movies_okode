0dbe775ad7ae859da13d0faad9f2fdb029dbf4cd
AdrianaFayos <adrifayosperis@gmail.com>
2021-07-28 08:33:22 +0200

    httpclient, observable and map imported at movie service

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { IMovies } from '../model/IMovies.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  searchMovies () {
+
+  }
+
+  getMovieDetail () {
+    
+  }
 }
